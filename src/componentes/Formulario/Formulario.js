a3adca706f2b3f393d714ecf4c568b31b783ab79
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-15 18:04:10 -0500

    Formulario -  Componente boton/evitando que recarge la pagina

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -5,8 +5,12 @@ import Boton from "../Boton";
 
 
 const Formulario = ()=>{
+    const manejarEnvio = (e) =>{
+        e.preventDefault();
+    }
+    
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
             <CampoTexto titulo="Nombre" placeholder="Ingrese el Nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingrese el Puesto"/>
