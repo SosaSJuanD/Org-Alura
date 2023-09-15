1f74e8ddbc22dbceff89d02b501f9f5496046aba
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-15 15:47:38 -0500

    encabezado formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './componentes/Header/Header';
+import Formulario from './componentes/Formulario/Formulario';
 
 function App() {
   return (
     <div>
       {/* {Header()}podemos hacer el llamado de tres maneras  */}
       {/* <Header></Header>podemos hacer el llamado de tres maneras   */}
-      <Header />{/* podemos hacer el llamado de tres maneras  */}
+      <Header/>{/* podemos hacer el llamado de tres maneras  */}
+      <Formulario/>
     </div>
   );
 }
