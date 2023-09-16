136af3059401e0a834dd0f932acd650f3bea8c0f
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-15 19:22:16 -0500

    Formulario -  Campos requeridos

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,7 +3,7 @@ import "./CampoTexto.css"
 const CampoTexto = (props) =>{
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder = {props.placeholder}/> {/* buenas practicas para cerrar las ET input en React */}
+        <input placeholder = {props.placeholder} required={props.required} /> {/* buenas practicas para cerrar las ET input en React */}
     </div>
 }
 
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -12,9 +12,9 @@ const Formulario = ()=>{
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingrese el Nombre"/>
-            <CampoTexto titulo="Puesto" placeholder="Ingrese el Puesto"/>
-            <CampoTexto titulo="Foto" placeholder="Ingrese enlace de Foto"/>
+            <CampoTexto titulo="Nombre" placeholder="Ingrese el Nombre" required/>
+            <CampoTexto titulo="Puesto" placeholder="Ingrese el Puesto" required/>
+            <CampoTexto titulo="Foto" placeholder="Ingrese enlace de Foto" required/>
             <ListaOpciones/>
             <Boton texto="Crear"/>
         </form>
