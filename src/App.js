aacfa0c6ccff2ea51cefcb2aec76ad01df070f9f
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-18 15:53:06 -0500

    Equipo - Manejo correcto de la informacion de los equipos / estilos basicos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,45 @@ function App() {
     actulizarMostar(!mostrarFormulario)
   }
 
+  //Lista de Equipos
+  const equipos =[
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9",
+    },
+    {
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF",
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2",
+    },
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8",
+    },
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5",
+    },
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9",
+    },
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF",
+    },
+  ]
+
   return (
     <div>
       {/* {Header()}podemos hacer el llamado de tres maneras  */}
@@ -25,13 +64,12 @@ function App() {
       {/* {mostrarFormulario ? <Formulario/> : <></>} */}
       {mostrarFormulario && <Formulario/>}
       <MiOrg CambiarMostrar={CambiarMostrar}/>
-      <Equipo equipo="Programación"/>
-      <Equipo equipo="Front End"/>
-      <Equipo equipo="Data Science"/>
-      <Equipo equipo="Devops"/>
-      <Equipo equipo="UX y Diseño"/>
-      <Equipo equipo="Móvil"/>
-      <Equipo equipo="Innovación y  Gestión"/>
+      {/* siempre que trabajemos con map debemos hacer uso de key */}
+      {
+        equipos.map( (equipo) =>{
+          return <Equipo datos={equipo} key={equipo.titulo}/>
+        } )
+      }
     </div>
   );
 }
--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,8 +1,18 @@
 import "./Equipo.css"
 
 const Equipo = (props) =>{
-    return <section className="equipo">
-        <h3>{props.equipo}</h3>
+    // destruccturacion
+    const {colorPrimario, colorSecundario, titulo} = props.datos;
+    const obj ={
+        backgroundColor: colorSecundario,
+    }
+
+    const estiloTitulo={
+        borderColor: colorSecundario,
+    }
+
+    return <section className="equipo" style={obj}>
+        <h3 style={estiloTitulo}> {titulo} </h3>
         <div className="colaboradores">
 
         </div>
