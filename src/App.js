4f90ecadf5f3999d9afd4e75b0c2794d41808794
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-17 22:04:44 -0500

    MiOrg - Uso de hooks para mostrar o no el formulario

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
 
 function App() {
+  const [mostrarFormulario, actulizarMostar ] = useState(true)
+  //Ternarios --> condicion ? seMuestra : noSemuestra
+
+  const CambiarMostrar = () =>{
+    actulizarMostar(!mostrarFormulario)
+  }
+
   return (
     <div>
       {/* {Header()}podemos hacer el llamado de tres maneras  */}
       {/* <Header></Header>podemos hacer el llamado de tres maneras   */}
       <Header/>{/* podemos hacer el llamado de tres maneras  */}
-      <Formulario/>
-      <MiOrg/>
+      {mostrarFormulario === true ? <Formulario/> : <div></div>}
+      <MiOrg CambiarMostrar={CambiarMostrar}/>
     </div>
   );
 }
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,10 +1,22 @@
+import {useState} from "react"
 import "./MiOrg.css"
 
 
-const MiOrg = () => {
+const MiOrg = (props) => {
+    //Estado - hooks
+    //hool = useState
+    //useState()
+    //const [nombreVariable, funcionActuliza] = useState(valorInicial);
+    //como le damos el uso en el mismo archivo
+    // const [mostrar, actuzalixarMostrar] = useState(true)
+    // const manejarClick = () =>{
+    //     console.log("Mostrar elemento", !mostrar)
+    //     actuzalixarMostrar(mostrar);
+    // }
+    //el uso adeciado a trabes de props
     return <section className="orgSection">
         <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add"/>
+        <img src="/img/add.png" alt="add" onClick={props.CambiarMostrar}/>
     </section>
 }
 
