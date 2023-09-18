724250810867d30f17bb59a5db79430747b989f8
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-17 23:02:49 -0500

    Campo texto - manejo basico de inputs

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,21 @@
+import { useState } from "react"
 import "./CampoTexto.css"
 
 const CampoTexto = (props) =>{
+    const [valor, actualizarValor] = useState("")
+    
+    const manejarCambio = (e) =>{
+        actualizarValor(e.target.value)
+    }
+
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder = {props.placeholder} required={props.required} /> {/* buenas practicas para cerrar las ET input en React */}
+        <input 
+            placeholder = {props.placeholder} 
+            required={props.required} 
+            value={valor}
+            onChange={manejarCambio}
+        /> {/* buenas practicas para cerrar las ET input en React */}
     </div>
 }
 
