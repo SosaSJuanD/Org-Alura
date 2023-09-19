1537a1622fb229090859150d6761994b47ef1dd6
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-18 21:55:11 -0500

    Colaborador/ Footer - Datos iniciales de equipos - Creacion del componente Footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,42 @@ import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
 import Equipo from './componentes/Equipo';
 import Colaborador from './componentes/Colaborador';
+import Footer from './componentes/Footer';
 
 function App() {
   const [mostrarFormulario, actulizarMostar ] = useState(false)
-  const [colaboradores, actualizarColaboradores] = useState([])
+  const [colaboradores, actualizarColaboradores] = useState([
+    {
+      equipo: "Front End",
+      foto: "https://github.com/harlandlohora.png",
+      nombre: "Harland Lohora",
+      puesto: "Instructor"
+    },
+    {
+      equipo: "Programación",
+      foto: "https://github.com/genesysaluralatam.png",
+      nombre: "Genesys Rondón",
+      puesto: "Desarrolladora de software e instructora"
+    },
+    {
+      equipo: "UX y Diseño",
+      foto: "https://github.com/JeanmarieAluraLatam.png",
+      nombre: "Jeanmarie Quijada",
+      puesto: "Instructora en Alura Latam"
+    },
+    {
+      equipo: "Programación",
+      foto: "https://github.com/christianpva.png",
+      nombre: "Christian Velasco",
+      puesto: "Head de Alura e Instructor"
+    },
+    {
+      equipo: "Innovación y Gestión",
+      foto: "https://github.com/JoseDarioGonzalezCha.png",
+      nombre: "Jose Gonzalez",
+      puesto: "Dev FullStack"
+    }
+  ])
 
 
 
@@ -97,6 +129,8 @@ function App() {
           />
         )
       }
+
+      <Footer/>
     </div>
   );
 }
