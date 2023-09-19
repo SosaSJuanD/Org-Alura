c9332b0498a30ab9ffd36632de6fb66ae9e55491
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-19 00:46:16 -0500

    Colaborador - creando input colores fondo y obteniendo valores hexa

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -21,6 +21,14 @@ const Equipo = (props) =>{
         {
             colaboradores.length > 0 &&
             <section className="equipo" style={obj}>
+                <input
+                    type="color"
+                    className="input-color"
+                    value={colorSecundario}
+                    onChange={(evento)=>{
+                        console.log(evento.target.value)
+                    }}
+                />
                 <h3 style={estiloTitulo}> {titulo} </h3>
                 <div className="colaboradores">
                     {
